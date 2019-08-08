0057f800e1149c3592d62f7da99b383a8ea4baf0
sivasakthisakthivel <53892998+sivasakthisakthivel@users.noreply.github.com>
2019-08-08 14:51:56 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,17 @@
+for a in string: 
+    if (a.isnumeric()) == True: 
+        count+= 1
+    else: 
+        newstring1+= a 
+print(count) 
+print(newstring1) 
+   
+string ='123ayu456'
+count = 0
+for a in string: 
+    if (a.isnumeric()) == True: 
+        count+= 1
+    else: 
+        newstring2+= a 
+print(count) 
+print(newstring2) 
