64c2a070ac613f65e6841ec4862d724822880e7b
sivasakthisakthivel <53892998+sivasakthisakthivel@users.noreply.github.com>
2019-08-08 14:55:18 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,22 @@
+List = [] 
+print("Intial blank List: ") 
+print(List) 
+  
+# Creating a List with  
+# the use of a String 
+List = ['GeeksForGeeks'] 
+print("\nList with the use of String: ") 
+print(List) 
+  
+# Creating a List with 
+# the use of multiple values 
+List = ["Geeks", "For", "Geeks"] 
+print("\nList containing multiple values: ") 
+print(List[0])  
+print(List[2]) 
+  
+# Creating a Multi-Dimensional List 
+# (By Nesting a list inside a List) 
+List = [['Geeks', 'For'] , ['Geeks']] 
+print("\nMulti-Dimensional List: ") 
+print(List) 
